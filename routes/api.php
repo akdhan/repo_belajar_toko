55f2726dca1a0e5c38b9a6cba23e6c6af462e200
akdhan <akdhaanwedyaharjuna@gmail.com>
2020-09-28 11:27:20 +0700

    Menambah autensikasi API dengan JWT

--- routes/api.php
+++ routes/api.php
@@ -2,21 +2,11 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
+Route::post('/register', 'UserController@register');
+Route::post('/login', 'UserController@login');
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
-
+Route::group(['middleware' => ['jwt.verify']], function()
+{
 Route::get('/customers', 'CustomersController@show');
 Route::post('/customers', 'CustomersController@store');
 Route::put('/customers/{id}', 'CustomersController@update');
@@ -38,4 +28,4 @@ Route::get('/parkir/{id}', 'ParkirController@detail');
 Route::post('/parkir', 'ParkirController@store');
 Route::put('/parkir/{id}', 'ParkirController@update');
 Route::delete('/parkir/{id}', 'ParkirController@destroy');
-
+});
